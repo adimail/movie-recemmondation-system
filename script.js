aa1188d76c3983dc8baf027d6407990e9eefd0e9
Aditya Godse <adimail2404@gmail.com>
2023-05-20 16:12:55 +0530

    Updated Files

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 function fetchMovies() {
     const directorName = document.getElementById("directorName").value;
-    const apiKey = "YOUR_API_KEY"; // Replace with your TMDb API key
+    const apiKey = "978e5c7a7fe49a1a6496279638088e9c"; // Replace with your TMDb API key
     const apiUrl = `https://api.themoviedb.org/3/search/person?api_key=${apiKey}&query=${directorName}`;
   
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
         if (data.results.length > 0) {
-          const directorId = data.results[0].id;
+          const director = data.results[0];
+          const directorId = director.id;
           const movieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_cast=${directorId}&sort_by=vote_average.desc`;
   
           fetch(movieUrl)
             .then(response => response.json())
             .then(movieData => {
               if (movieData.results.length > 0) {
-                const movies = movieData.results;
+                const movies = movieData.results.slice(0, 7); // Display only top 5 movies
                 let output = "<h2>Top Movies:</h2>";
+                output += `<div><img src="https://image.tmdb.org/t/p/w200/${director.profile_path}" alt="${director.name}"></div>`; // Display director's image
                 output += "<ul>";
                 for (let i = 0; i < movies.length; i++) {
                   const movie = movies[i];
